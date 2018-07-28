a3baf2b96be357c22dc9391cf87a3b0527b528b7
soboro1207 <kd1207839@st.kobedenshi.ac.jp>
2018-07-28 22:06:39 +0900

    commit

--- vue.js
+++ vue.js
@@ -67,3 +67,28 @@ var app7 = new Vue({
     ]
   }
 })
+
+var app = new Vue({
+  el: '#todoApp',
+  data: {
+    title: "Welcome to Vue.js",
+    fontStyle: {
+      fontSize: "20px",
+    },
+    todo: "",
+    todos: []
+  },
+  methods: {
+    addTodo: function(){
+      this.todos.push({
+        id: this.todos.length,
+        name: this.todo,
+        doing: false
+      })
+      this.todo = ""
+    },
+    begin: function(todo){
+      todo.doing = !todo.doing;
+    }
+  }
+})
