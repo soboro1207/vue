391439a528fdb8b5381f562e0928d33cc171e38f
soboro1207 <kd1207839@st.kobedenshi.ac.jp>
2018-08-06 22:06:28 +0900

    Vue Practice days3

--- js/main.js
+++ js/main.js
@@ -7,16 +7,16 @@ var app = new Vue({
   el: '#app',
 //2.アプリケーションで使用するデータ
   data: {
-    message: 'Hello Vue',
-    list: ['apple', 'banana', 'strowbarry'],
-    show: true
+    message: null,
+    scrollY: 0,
+    timer: null
   },
 //3.算出プロパティ
-  computed: {
-    computedMessage: function(){
-      return this.message + '!'
-    }
-  },
+  // computed: {
+  //   computedMessage: function(){
+  //     return this.message + '!'
+  //   }
+  // },
 //4.ライフサイクルフック
 /*Vue.jsのおはようからおやすみまでの一定のサイクルを表すもの
 * あらかじめ登録した処理をVueインスタンスの特定のタイミングで自動的に呼び出す。
@@ -26,10 +26,25 @@ var app = new Vue({
     //データの監視、リアクティブまわりの初期化が終わった後に呼び出される
     //このインスタンスの作成＆初期化が終わったらすぐ以下の処理を行う
     //使用できるメソッドがあらかじめ決められている
+    window.addEventListener('scroll', this.handleScroll)
   },
-  
+  beforeDestroy: function(){
+    window.removeEventListener('scroll', this.handleScroll)
+  },
 //5.アプリケーションで使用するメソッド
   methods: {
+    handler: function(comment){
+      console.log(comment)
+    },
+    handleScroll: function(){
+      if(this.timer==null){
+        this.timer=setTimeout(function(){
+          this.scrollY = window.scrollY
+          clearTimeout(this.timer)
+          this.timer=null
+        }.bind(this), 200)
+      }
+    }
     /*myMethod: function(){}*/
   }
 })
