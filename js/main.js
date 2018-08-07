27d761e1f5f75eaecebcc079e0e72d8327de71be
soboro1207 <kd1207839@st.kobedenshi.ac.jp>
2018-08-07 20:47:10 +0900

    Vue Practice days4

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var app = new Vue({
+new Vue({
 /*基本的な構成
 * なんとなくこんな形になる
 */
@@ -7,11 +7,23 @@ var app = new Vue({
   el: '#app',
 //2.アプリケーションで使用するデータ
   data: {
-    message: null,
-    scrollY: 0,
-    timer: null
+    list: [],
+    current: '',
+    topics: [
+      { value: 'vue', name: 'Vue.js' },
+      { value: 'jQuery', name: 'jQuery' }
+    ]
   },
 //3.算出プロパティ
+watch: {
+  current: function(value){  //監視するデータ: function(新しい値、古い値)
+    axios.get('https://api.github.com/search/repositories',{
+      params: { q: 'topic:' + value }
+    }).then(function(response){
+      this.list = response.data.items
+    }.bind(this))
+  }
+},
   // computed: {
   //   computedMessage: function(){
   //     return this.message + '!'
@@ -22,29 +34,12 @@ var app = new Vue({
 * あらかじめ登録した処理をVueインスタンスの特定のタイミングで自動的に呼び出す。
 * こうした処理を割り込ませる仕組みのことをフックと呼ぶ
 */
-  created: function(){
+  //created: function(){
     //データの監視、リアクティブまわりの初期化が終わった後に呼び出される
     //このインスタンスの作成＆初期化が終わったらすぐ以下の処理を行う
     //使用できるメソッドがあらかじめ決められている
-    window.addEventListener('scroll', this.handleScroll)
-  },
-  beforeDestroy: function(){
-    window.removeEventListener('scroll', this.handleScroll)
-  },
+
+    //}
 //5.アプリケーションで使用するメソッド
-  methods: {
-    handler: function(comment){
-      console.log(comment)
-    },
-    handleScroll: function(){
-      if(this.timer==null){
-        this.timer=setTimeout(function(){
-          this.scrollY = window.scrollY
-          clearTimeout(this.timer)
-          this.timer=null
-        }.bind(this), 200)
-      }
-    }
     /*myMethod: function(){}*/
-  }
 })
